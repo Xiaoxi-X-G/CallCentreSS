2d52826dc290d7acceddd6cd275fd97b927b1920
Xiaoxi Guo <xguo@time2work.com>
2016-09-15 12:21:29 +1000

    log -> BoxCox -> SS, Loess=24

--- GeneralStatistics.R
+++ GeneralStatistics.R
@@ -28,144 +28,185 @@ DataAllClean$Items <- as.numeric(DataAllClean$Items)
  
 
 
-######### Check Intra and Inter day correlation ####
-require(corrgram)
-require(corrplot)
-
-### Inter day
-Corrgram.data.inter.temp <- DataAllClean
-Corrgram.data.inter.temp[,1] <- as.POSIXct(Corrgram.data.inter.temp[,1], origin = "1970-01-01", tz="GMT")
-
-Corrgram.data.inter <- tapply(as.integer(Corrgram.data.inter.temp$Items),
-                              as.factor(format(Corrgram.data.inter.temp$DateTime, "%Y-%m-%d")), sum)
-
-Corrgram.data.inter <- aggregate(as.integer(Corrgram.data.inter.temp$Items), 
-          list(Date=format(Corrgram.data.inter.temp$DateTime, "%Y-%m-%d")), 
-          FUN=sum)
-colnames(Corrgram.data.inter)[2] <- "Value"
-
-Corrgram.data.inter$WkDay <- weekdays(as.Date(Corrgram.data.inter$Date))
-#Corrgram.data.inter$WkDay <- as.numeric(as.factor(Corrgram.data.inter$WkDay) )
-
-
-boxplot(Corrgram.data.inter$Value ~ Corrgram.data.inter$WkDay)
-
-Corrgram.matrix.inter <- t(matrix(Corrgram.data.inter$Value[c(1:(7*floor(nrow(Corrgram.data.inter)/7)))], nrow  = 7))
-colnames(Corrgram.matrix.inter) <- head(Corrgram.data.inter$WkDay,n=7)
-
-
-# m1
-corrgram(Corrgram.matrix.inter, order = F,
-         upper.panel = panel.pie,
-         lower.panel = panel.conf)
-
-
-# m2
-Correlation.inter <- cor(Corrgram.matrix.inter)
-corrplot(Correlation.inter, order = "hclust")
-
-
-### Intra
-Corrgram.data.intra.temp <- DataAllClean
-Corrgram.matrix.intra <- t(matrix(as.numeric(Corrgram.data.intra.temp$Items), nrow  = 24*60/as.integer(Interval)))
-
-colnames(Corrgram.matrix.intra) <- seq(from =0, by=as.integer(Interval)/60,
-                                       length = 60/as.integer(Interval)*24)
-# m1
-corrgram(Corrgram.matrix.intra, order = FALSE,
-         upper.panel = panel.pie,
-         lower.panel = panel.conf,
-         cor.method="pearson")
-
-
-# m2
-Correlation.intra <- cor(Corrgram.matrix.intra)
-corrplot(Correlation.intra, order = "hclust")
+# ######### Check Intra and Inter day correlation ####
+# require(corrgram)
+# require(corrplot)
+# 
+# ### Inter day
+# Corrgram.data.inter.temp <- DataAllClean
+# Corrgram.data.inter.temp[,1] <- as.POSIXct(Corrgram.data.inter.temp[,1], origin = "1970-01-01", tz="GMT")
+# 
+# Corrgram.data.inter <- tapply(as.integer(Corrgram.data.inter.temp$Items),
+#                               as.factor(format(Corrgram.data.inter.temp$DateTime, "%Y-%m-%d")), sum)
+# 
+# Corrgram.data.inter <- aggregate(as.integer(Corrgram.data.inter.temp$Items), 
+#           list(Date=format(Corrgram.data.inter.temp$DateTime, "%Y-%m-%d")), 
+#           FUN=sum)
+# colnames(Corrgram.data.inter)[2] <- "Value"
+# 
+# Corrgram.data.inter$WkDay <- weekdays(as.Date(Corrgram.data.inter$Date))
+# #Corrgram.data.inter$WkDay <- as.numeric(as.factor(Corrgram.data.inter$WkDay) )
+# 
+# 
+# boxplot(Corrgram.data.inter$Value ~ Corrgram.data.inter$WkDay)
+# 
+# Corrgram.matrix.inter <- t(matrix(Corrgram.data.inter$Value[c(1:(7*floor(nrow(Corrgram.data.inter)/7)))], nrow  = 7))
+# colnames(Corrgram.matrix.inter) <- head(Corrgram.data.inter$WkDay,n=7)
+# 
+# 
+# # m1
+# corrgram(Corrgram.matrix.inter, order = F,
+#          upper.panel = panel.pie,
+#          lower.panel = panel.conf)
+# 
+# 
+# # m2
+# Correlation.inter <- cor(Corrgram.matrix.inter)
+# corrplot(Correlation.inter, order = "hclust")
+# 
+# 
+# ### Intra
+# Corrgram.data.intra.temp <- DataAllClean
+# Corrgram.matrix.intra <- t(matrix(as.numeric(Corrgram.data.intra.temp$Items), nrow  = 24*60/as.integer(Interval)))
+# 
+# colnames(Corrgram.matrix.intra) <- seq(from =0, by=as.integer(Interval)/60,
+#                                        length = 60/as.integer(Interval)*24)
+# # m1
+# corrgram(Corrgram.matrix.intra, order = FALSE,
+#          upper.panel = panel.pie,
+#          lower.panel = panel.conf,
+#          cor.method="pearson")
+# 
+# 
+# # m2
+# Correlation.intra <- cor(Corrgram.matrix.intra)
+# corrplot(Correlation.intra, order = "hclust")
 
 
 #### Segment for training and testing ####
-Test.Start <- "2013-03-01"
-wk.total <- 9
-DataClean <- DataAllClean[which((as.Date(DataAllClean$DateTime)>= as.Date(Test.Start))
-                                 & (as.Date(DataAllClean$DateTime)<= (as.Date(Test.Start)+wk.total*7-1))),]
-
-wk.training <- 7
+Training.End <- "2012-08-21"
+  
+wk.training <- 6
 wk.testing <- 2
-Data.training <- head(DataClean, n = wk.training*7*24*(60/as.integer(Interval)))
-Data.testing <- tail(DataClean, n = wk.testing*7*24*(60/as.integer(Interval)))
+Data.training <- DataAllClean[which((as.Date(DataAllClean$DateTime)>= (as.Date(Training.End)- wk.training*7+1 ))
+                            & (as.Date(DataAllClean$DateTime)<= as.Date(Training.End))),]
+
+Data.testing <- DataAllClean[which((as.Date(DataAllClean$DateTime)>= (as.Date(Training.End) + 1 ))
+                                    & (as.Date(DataAllClean$DateTime)<= (as.Date(Training.End) + 7*wk.testing))),]
 
 
 plot(c(Data.training$Items, rep(0, length= nrow(Data.testing))),
      type ="o", col= "blue", 
-     ylim=c(0, max(DataClean$Items)))
-lines(c(rep(0, length= nrow(Data.training)), Data.testing$Items), type = "o", pch = 22, lty = 2, col = "red")
+     ylim=c(0, max(Data.training$Items)),
+     main = "Training + Testing")
+lines(c(rep(0, length= nrow(Data.training)), Data.testing$Items), 
+      type = "o", pch = 22, lty = 2, col = "red")
 
 ###############
 #### preprocessing ####
 # 1. BoxCox
 require(MASS)
+require(forecast)
 
 LoessSmooth <- Data.training
 Lambda <- BoxCox.lambda(LoessSmooth$Items)
 LoessSmooth$BoxCox <- BoxCox(LoessSmooth$Items, Lambda)
+LoessSmooth$LogPlus1 <- log(LoessSmooth$Items+1)
+
+hist(LoessSmooth$Items)
+hist(LoessSmooth$BoxCox)
+hist(LoessSmooth$LogPlus1)
+
 
 #2. Smoothing
-lo <- loess(LoessSmooth$BoxCox ~ as.numeric(as.POSIXct(LoessSmooth$DateTime, origin = "1970-01-01", tz="GMT")),
-            span = 0.1)
-plot(LoessSmooth$BoxCox,   type ="o", col= "blue",  ylim=c(min(LoessSmooth$BoxCox), max(LoessSmooth$BoxCox)))
+NOPoint <- 24 # Define locate data-set, i.e., NOPoint/Polynormial
+alpha <- NOPoint/nrow(LoessSmooth)
+lo <- loess(LoessSmooth$LogPlus1 ~ as.numeric(as.POSIXct(LoessSmooth$DateTime, origin = "1970-01-01", tz="GMT")),
+            span = alpha,
+            parametric = F)
+plot(LoessSmooth$LogPlus1,   type ="o", col= "blue",  
+     ylim=c(0, max(LoessSmooth$LogPlus1)),
+     main= "Log + Loess")
 lines(lo$fitted, type = "o", pch = 22, lty = 2, col = "red")
 
+
+plot(LoessSmooth$Items, type="o", col = "blue", main="Orignal + Reversed Loess")
+lines(exp(lo$fitted)+1, type = "o", pch = 22, lty = 2, col = "red")
 
 
 
 #########################################
 #### State-Space Model ####
-require(forecast)
 #Input.data <- Data.training$Items
-Input.data <- lo$fitted
+Input.data <- as.numeric(lo$fitted)
 Seasonal1 <- 60*24/as.integer(Interval)
 Seasonal2 <- 7*Seasonal1
 Data.msts <- msts(Input.data, seasonal.periods = c(Seasonal1, Seasonal2))
-fit.tbats <- tbats(Data.msts, use.box.cox = T, use.trend = T, 
-                   use.damped.trend = T, use.arma.errors = T)
+Fit.tbats <- tbats(Data.msts, use.box.cox = T, 
+                   seasonal.periods = c(Seasonal1, Seasonal2),
+                   use.trend = T,  use.damped.trend= T,
+                   use.arma.errors = T)
 
 lg <- wk.testing*7*24*60/as.integer(Interval)
 
-Results.temp <- forecast(fit.tbats, h =lg)
+Results.temp <- forecast(Fit.tbats, h =lg)
 #Results <- Results.temp$mean
-Results <- InvBoxCox(Results.temp$mean, Lambda)
+#Results <- InvBoxCox(Results.temp$mean, Lambda)
+Results <- exp(as.numeric(Results.temp$mean)) - 1
 
 Results[which(Results < 0 )] <- 0
 
+Ck <- data.frame(Results.temp$mean, Exp = exp(Results.temp$mean) - 1, Results, Data.testing$Items)
+
+plot(c(lo$fitted, rep(0, length= nrow(Data.testing))),
+     type ="o", col= "blue",  ylim=c(0, max(lo$fitted)), cex.axis=1.5,
+     main = 'Loess + Predicted')
+lines(c(rep(0, length= nrow(Data.training)), Results.temp$mean), type = "o", pch = 22, lty = 2, col = "red")
+
+
+plot(c(exp(lo$fitted)-1, rep(0, length= nrow(Data.testing))),
+     type ="o", col= "blue",  cex.axis=1.5,
+     main = 'Exp_Loess + Exp_Predicted')
+lines(c(rep(0, length= nrow(Data.training)), Results), 
+      type = "o", pch = 22, lty = 2, col = "red")
+
+
 plot(c(Data.training$Items, rep(0, length= nrow(Data.testing))),
-     type ="o", col= "blue",  ylim=c(0, max(DataClean$Items)), cex.axis=1.5)
+     type ="o", col= "blue",  ylim=c(0, max(Data.training$Items)), cex.axis=1.5)
 lines(c(rep(0, length= nrow(Data.training)), Data.testing$Items), type = "o", pch = 22, lty = 2, col = "red")
 lines(c(rep(0, length= nrow(Data.training)), Results), type = "o", pch = 22,  col = "green")
+
 
 plot(Data.testing$Items, type = "o", col = "red")
 lines(as.numeric(Results), type = "o", pch = 22,  col = "green")
 
 
-RMSE <- sqrt(mean((Data.testing$Items-Results)^2, na.rm =T))
-
-ConfidenceLevel <- 0.90
-
-Data.testing$Up <- Data.testing$Items*(1+1- ConfidenceLevel)
-Data.testing$Lo <- Data.testing$Items*(ConfidenceLevel)
   
-Data.testing$CoverIndicator<- ((Results <= (Data.testing$Items*(1+ 1-ConfidenceLevel)))
-                                    & (Results >= (Data.testing$Items*ConfidenceLevel)))
+######## Residual check - per hours
+RMSE <- sqrt(mean((Data.testing$Items-Results)^2, na.rm =T))
 Data.testing$Pred <- Results
 
-  
-######## Residual check
 Residual <- Data.testing$Pred - Data.testing$Items
 plot(Residual)
 mean(Residual)
+hist(Residual)
 acf(Residual)
 qqnorm(Residual)
 
-1-mean(abs(Data.testing$Pred - Data.testing$Items)/mean(Data.testing$Items))
+mean(1-abs(Data.testing$Pred - Data.testing$Items)/max(Data.testing$Items, rm.na=T), rm.na=T)
+
+
+
+### Daily error
+DailyResult <- aggregate(Data.testing[,c(2,3)], 
+                list(format(as.POSIXct(Data.testing$DateTime, origin = "1970-01-01", tz = "GMT"), "%Y-%m-%d")), 
+                 sum)
+
+colnames(DailyResult)[1] <- "Date"
+
+DailyResult$Residual <- DailyResult$Items - DailyResult$Pred
+mean(1 - abs(DailyResult$Residual)/ max(DailyResult$Items))
+
 
 # FormatTS <- function(DataAll, FirstDate, LastDate, Interval){
 # ### Format the time series DataAll with fixed Interval from FirstDate to LastDate
