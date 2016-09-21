5702cdd99fd748cebdd0e6e0cf02cdf580310dea
Xiaoxi Guo <xguo@time2work.com>
2016-09-21 12:19:58 +1000

    create NormalIntradayPrediction_LargeCalls

--- GeneralStatistics.R
+++ GeneralStatistics.R
@@ -126,7 +126,56 @@ if (mean(Data.training.daily$Value, na.rm = T) < 100){
   
 }
   
+NormalIntradayPrediction_LargeCalls <- function(Data.training, lg, Interval){
+  # Data.training = dataframe(DateTime, Items), that is cleaned data with fixed interval
+  # lg = no. of days to forecast, that starts from next unavailable day
+  
+  # output = lg*60*24 numeric data
+  
+  ############################################
+  ### 1. BoxCox transformation
+  ### 2. Intraday prediction
+  ### 3. Inverse BoxCox
+  ############################################
+  
+  
+  ### 1. BoxCox transformation
+  Input.data <- Data.training
+  Lambda <- BoxCox.lambda(Input.data$Items)
+  Input.data$BoxCox <- BoxCox(Input.data$Items, Lambda)
+  
+  
+  ### 2. Intraday prediction
+  Fit <- tryCatch(
+    {
+      Seasonal1 <- 60*24/as.integer(Interval)
+      Seasonal2 <- 7*Seasonal1
+      Data.msts <- msts(Input.data$BoxCox, seasonal.periods = c(Seasonal1, Seasonal2))
+      Fit <- tbats(Data.msts, use.box.cox = F, 
+                         seasonal.periods = c(Seasonal1, Seasonal2),
+                         use.trend = T,  use.damped.trend= T,
+                         use.arma.errors = T)
+    },
+    warning = function(cond){
+      Data.ts <- ts(Input.data$BoxCox, frequency = 7*24*60/as.integer(Interval))
+      Fit <- ets(Data.ts)
+      return(Fit)
+    },
+    error = function(cond){
+      Data.ts <- ts(Input.data$BoxCox, frequency = 7*24*60/as.integer(Interval))
+      Fit <- ets(Data.ts)
+      return(Fit)
+    }
+  )
+  
+  Results.temp <- forecast(Fit, h =lg)
 
+  ### 3. Inverse BoxCox
+  Results <- InvBoxCox(as.numeric(Results.temp$mean), Lambda)
+  Results[which(Results < 0)] <- 0 
+  
+  return(Results)
+}
 
 
 ###############
