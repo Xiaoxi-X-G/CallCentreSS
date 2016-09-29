3da010ff9f29291f51e440fc61d54757a767f08b
Xiaoxi Guo <xguo@time2work.com>
2016-09-29 14:39:24 +1000

    ck

--- GeneralStatistics.R
+++ GeneralStatistics.R
@@ -199,38 +199,12 @@ DatabaseName<-"Time2Work_EZCorp"
 LocationID <- 9
 ## Note: use Ezcorp database, assuming exist the same format
 require(chron)
-
 OpenDayTime <- OpenCloseDayTime(StartDate, FinishDate, LocationID,RScriptPath, DatabaseName)
 
 Results.scaled.finial <- ResultScaled(Results.finial, OpenDayTime, StartDate, FinishDate, Interval)
 
 
-
-# 
-# Results.finial.matrix <- t(matrix(Results.finial, nrow = 24*60/as.integer(Interval)))
-# colnames(Results.finial.matrix) <- format(seq(as.POSIXct(paste(StartDate, "00:00:00"), origin="1970-01-01", tz="GMT"),
-#                                               by = paste(Interval, "mins"),
-#                                               length.out = 60*24/as.integer(Interval)), "%H:%M:%S")
-# 
-# rownames(Results.finial.matrix) <- as.character(seq(as.Date(StartDate), as.Date(FinishDate), by = "1 day"))
-# Results.scaled.matrix <- matrix(rep(0, length=length(Results.finial)), 
-#                                 ncol = 24*60/as.integer(Interval))
-# for (d in 1:length(OpenDayTime$Dates)){
-#   if ((OpenDayTime[d, 2] == "00:00:00") & (OpenDayTime[d, 3] == "00:00:00")){ # close days
-#     next
-#   }else{ # otherwise
-#     Ind <- which( chron(times = colnames(Results.finial.matrix)) >= chron(times = OpenDayTime[d,2])
-#                   & chron(times = colnames(Results.finial.matrix)) <= chron(times = OpenDayTime[d,3]))
-#     Results.scaled.matrix[d, Ind] <- (sum(Results.finial.matrix[d,]) / sum(Results.finial.matrix[d, Ind]))*Results.finial.matrix[d, Ind]
-#   }
-# }
-# 
-# 
-# Results.scaled.finial <- as.vector(t(Results.scaled.matrix))
-
-
-
-
+###### Format output
 Results.finial.format <- 
   data.frame(DateTime = seq(as.POSIXct(paste(StartDate, "00:00:00"), origin="1970-01-01", tz="GMT"),
                             by = paste(Interval, "mins"),
